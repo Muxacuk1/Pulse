5585fe924b7855b37e46327becfe84c027b1a60b
Myhailo <mvlol02@gmail.com>
2023-01-24 22:04:16 +0200

    making carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
+        adaptiveHeight: true,
         prevArrow:'<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="../img/right.png"></button>'
       });
