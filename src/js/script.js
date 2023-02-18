efcf0150dd410d1139398ee185ef3aa1482524e9
Myhailo <mvlol02@gmail.com>
2023-02-19 00:00:11 +0200

    maked modal

--- src/js/script.js
+++ src/js/script.js
@@ -39,5 +39,23 @@ $(document).ready(function(){
     };
     toggleSlide ('.catalog-item__link');
     toggleSlide ('.catalog-item__back');
+
+    //model
+    $('[data-model="consultation"]').on('click', function(){
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay, #consultation, #thanks, #order'). fadeOut();
+    });
+    // $('.button_mini').on('click', function(){
+    //   $('.overlay, #order').fadeIn('slow');
+    // });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitel').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+    });
     
   });
